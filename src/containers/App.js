2ab670ff5c7878c210dc2490abdbb23dedd01687
MattV <mattvinicius.contato@gmail.com>
2023-03-22 22:36:20 -0300

    Transformed the class app to hook function

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import MediaQuery from "react-responsive";
 import CardList from "../components/cardList/cardList";
 import SearchBox from "../components/searchBox/searchBox";
@@ -6,59 +6,50 @@ import Scroll from "../components/scroll/scroll";
 import "./App.css";
 import ErrorBoundary from "../components/errorBoundry";
 
-//Smart Component
-class app extends Component {
-  //adding state
-  constructor(props) {
-    super(props); // calls the constructor // MUST HAVE
-    this.state = {
-      // describes our app // usually states live here in the parent component
-      robots: [],
-      searchField: "",
-    };
-  }
+// NOTE: THE HOOK FUNCTION NAME MUST BE CAPITALIZED!!!
+export default function App() {
+  //robots is the state and set robots the function that changes the state
+  const [robots, setRobots] = useState([]); // <- initial state
+  const [searchfield, setSearchfield] = useState(" "); // each one has a state
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((data) => this.setState({ robots: data }));
-  }
+      .then((data) => {
+        setRobots(data);
+      });
+  }, []); // use a empty array to useEffect once in the begging
 
-  onSearchChange = (event) => {
-    // how to update states
-    this.setState({ searchField: event.target.value });
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
   };
 
-  render() {
-    const { robots, searchField } = this.state;
-    const filteredRobots = robots.filter((robots) =>
-      robots.name.toLowerCase().includes(searchField.toLowerCase())
+  const filteredRobots = robots.filter((robot) =>
+    robot.name.toLowerCase().includes(searchfield.toLowerCase())
+  );
+
+  if (robots.length > 0) {
+    return (
+      <div className="tc">
+        <MediaQuery maxWidth={767}>
+          <h1 className="f2 lh-title grow dib br3 ma3 pa2 shadow-3">
+            ROBOFRIENDS
+          </h1>
+        </MediaQuery>
+        <MediaQuery minWidth={767}>
+          <h1 className="f-headline grow dib br3 ma3 pa2 shadow-3">
+            ROBOFRIENDS
+          </h1>
+        </MediaQuery>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
+        </Scroll>
+      </div>
     );
-    if (robots.length > 0) {
-      return (
-        <div className="tc">
-          <MediaQuery maxWidth={767}>
-            <h1 className="f2 lh-title grow dib br3 ma3 pa2 shadow-3">
-              ROBOFRIENDS
-            </h1>
-          </MediaQuery>
-          <MediaQuery minWidth={767}>
-            <h1 className="f-headline grow dib br3 ma3 pa2 shadow-3">
-              ROBOFRIENDS
-            </h1>
-          </MediaQuery>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <ErrorBoundary>
-              <CardList robots={filteredRobots} />
-            </ErrorBoundary>
-          </Scroll>
-        </div>
-      );
-    } else {
-      return <h1 className="tc">Loading...</h1>;
-    }
+  } else {
+    return <h1 className="tc">Loading...</h1>;
   }
 }
-
-export default app;
